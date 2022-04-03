34d4d913a70c0eebe39c48c7f3d7d53ac4c1cf87
admin <admin@adminnoMacBook.local>
2022-04-04 07:49:59 +0900

    refactor creating error hash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,22 +15,21 @@ class ApplicationController < ActionController::API
   private 
   
   def authentication_error
-    error = {
-      status: "401",
-      source: { pointer: "/code" },
-      title:  "Authentication code is invalid",
-      detail: "You must provide valid code in order to exchange it for token."
-    }
+    error = error_hash("401", "/code", "Authentication code is invalid", "You must provide valid code in order to exchange it for token.")
     render json: { "errors": [ error ] }, status: 401
   end
 
   def authorization_error
-    error = {
-      status: "403",
-      source: { pointer: "/headers/authorization" },
-      title:  "Not authorized",
-      detail: "You have no right to access this resource."
-    }
+    error = error_hash("403", "/headers/authorization", "Not authorized", "You have no right to access this resource.")
     render json: { "errors": [ error ] }, status: 403
+  end
+
+  def error_hash (status, source, title, detail)
+    return {
+      status: status,
+      source: { pointer: source },
+      title:  title,
+      detail: detail
+    }
   end
 end
